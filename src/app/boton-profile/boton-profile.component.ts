a3c7b46843945073a12eff273c5a788bbfef2031
gastyft <gastyftacla1@hotmail.com>
2022-06-15 08:22:22 -0300

    version 3

--- src/app/boton-profile/boton-profile.component.ts
+++ src/app/boton-profile/boton-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 import { EquipoService} from '../equipo.service';
 import {FormsModule } from '@angular/forms';
 @Component({
@@ -11,7 +11,7 @@ export class BotonProfileComponent implements OnInit {
 
   miPortfolio: any;
   
-  binding:string ="";
+
   
     constructor( 
       private datosPorfolio: EquipoService
@@ -21,8 +21,25 @@ export class BotonProfileComponent implements OnInit {
       this.datosPorfolio.ObtenerDatos().subscribe( (data: any) =>{
         console.log(data)
         this.miPortfolio=data;
+      })
+      {
+    this.datosPorfolio.EditarDatos().subscribe((data:any)=>{
+    this.miPortfolio= data;
+
     })
-   
+  }
+  {
+    this.datosPorfolio.BorrarDatos().subscribe((data:any)=>{
+      this.miPortfolio= data;
+
+    })
+  }
+ {
+    this.datosPorfolio.GuardarDatos().subscribe((data:any)=>{
+      this.miPortfolio= data;
+
+    })
+  }
   }
 
 }
